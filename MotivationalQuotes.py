73f24a50e8801a07992b5213a160cb1797b9a4fb
owenkoster <54965606+owenkoster@users.noreply.github.com>
2025-11-15 17:01:15 -0800

    worked on the microservice

--- MotivationalQuotes.py
+++ MotivationalQuotes.py
@@ -5,8 +5,45 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-def getQuotes(quantity, category):
-    return "Test Test"
+with open("Music.txt", 'r') as f: musicQuotes = f.readlines()
+with open("Pets.txt", 'r') as f: petsQuotes = f.readlines()
+with open("School.txt", 'r') as f: schoolQuotes = f.readlines()
+with open("Sports.txt", 'r') as f: sportsQuotes = f.readlines()
+with open("Games.txt", 'r') as f: GamesQuotes = f.readlines()
+
+def Get_Quote(category):
+    
+    if(category == ""):
+        rand = random.randint(0,4)
+        category = rand
+    else:
+        category = category.lower()
+    
+    if(category == "music" or category == 0):
+        quotes = musicQuotes
+    elif(category == "pets" or category == 1):
+        quotes = petsQuotes
+    elif(category == "school" or category == 2):
+        quotes = schoolQuotes
+    elif(category == "sports" or category == 3):
+        quotes = sportsQuotes
+    elif(category == "video games" or category == 4):
+        quotes = GamesQuotes
+    else:
+        quotes = GamesQuotes
+    return random.choice(quotes).strip()
+
+def Get_Quotes(quantity, category):
+    quotes = []
+    if(category == ""):
+        for ii in range(int(quantity)):
+            quotes.append(Get_Quote(""))
+    else:
+        for ii in range(int(quantity)):
+            quotes.append(Get_Quote(category))
+
+
+    return quotes
 
 while(True):
     message = socket.recv()
@@ -17,6 +54,7 @@ while(True):
         
         if len(s) > 1:
             category = s[1]
-            
-        quote = getQuotes(quantity, category)
-        socket.send_string(quote)
+            quotes = Get_Quotes(quantity, category)
+        else: 
+            quotes = Get_Quotes(quantity, "")
+        socket.send_multipart([l.encode('utf-8') for l in quotes])
--- TestClient.py
+++ TestClient.py
@@ -5,14 +5,15 @@ context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
-def get_quote(quantity, category = "random"):
+def Get_Quote(quantity, category):
     time.sleep(1)
     socket.send_string(f"{quantity} {category}")
-    quoteArray = socket.recv()
+    recieved = socket.recv_multipart()
+    quoteArray = [frame.decode('utf-8') for frame in recieved]
     return quoteArray
 
 while True:
     q = input("choose quantity of quotes: ")
     c = input("choose category of quote(or random): ")
-    noun = get_quote(q, c)
-    print(noun)
+    quotes = Get_Quote(q, c)
+    print(f"Received: {quotes}")
